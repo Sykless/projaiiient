fec3b7c97feb24868b8356b7f215932e6ca8a8fa
Sykless <rakouett-du-56@hotmail.fr>
2018-03-03 20:20:12 +0100

    Added Scroll

--- app/src/main/java/com/autoscroll/fraba/defiloche/MainActivity.java
+++ app/src/main/java/com/autoscroll/fraba/defiloche/MainActivity.java
@@ -1,8 +1,8 @@
 package com.autoscroll.fraba.defiloche;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -11,16 +11,12 @@ import android.graphics.Rect;
 import android.graphics.pdf.PdfRenderer;
 import android.os.ParcelFileDescriptor;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
@@ -29,7 +25,6 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
 import java.io.File;
 import java.util.ArrayList;
 
@@ -63,11 +58,9 @@ public class MainActivity extends AppCompatActivity {
     AnimatorSet animatorSet;
 
     boolean isRestarted = true;
+    boolean isRunning = false;
     boolean layoutSetup = false;
     boolean oneLine = true;
-    boolean twoLinesPacked = false;
-
-    float imagePosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -117,15 +110,22 @@ public class MainActivity extends AppCompatActivity {
             {
                 isRestarted = false;
 
-                if (movePartition.isPaused())
+                if (isRunning)
                 {
-                    playPauseButton.setImageResource(R.drawable.ic_pause_white_48dp);
-                    movePartition.resume();
+                    isRunning = false;
+                    playPauseButton.setImageResource(R.drawable.ic_play_arrow_white_48dp);
+
+                    animatorSet.cancel();
                 }
                 else
                 {
-                    playPauseButton.setImageResource(R.drawable.ic_play_arrow_white_48dp);
-                    movePartition.pause();
+                    isRunning = true;
+                    playPauseButton.setImageResource(R.drawable.ic_pause_white_48dp);
+
+                    movePartition = ObjectAnimator.ofInt(sView,"scrollY",sView.getScrollY(), sView.getBottom() + Math.round(endScreen));
+                    movePartition.setDuration(3000);
+                    animatorSet.play(movePartition);
+                    animatorSet.start();
                 }
             }
         });
@@ -141,12 +141,14 @@ public class MainActivity extends AppCompatActivity {
 
                 if (isRestarted)
                 {
-                    locationOnScreen[1] = 0;
+                    moveRestart = ObjectAnimator.ofInt(sView, "scrollY", Math.round(actionBarHeight), Math.round(actionBarHeight));
+                }
+                else
+                {
+                    moveRestart = ObjectAnimator.ofInt(sView, "scrollY", locationOnScreen[1], Math.round(actionBarHeight));
                 }
 
                 isRestarted = true;
-
-                moveRestart = ObjectAnimator.ofFloat(linearLayout, "translationY", (float) locationOnScreen[1], actionBarHeight);
 
                 animatorSet.play(moveRestart);
                 animatorSet.start();
@@ -158,6 +160,64 @@ public class MainActivity extends AppCompatActivity {
                 movePartition.pause();
             }
         });
+
+
+        Display display = getWindowManager().getDefaultDisplay();
+        size = new Point();
+        display.getSize(size);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            endScreen = (float) size.x;
+        }
+        else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            endScreen = (float) size.y;
+        }
+
+        linearLayout = (LinearLayout) findViewById(R.id.main_view);
+        sView = (ScrollView) findViewById(R.id.scroll);
+
+        File pdfFile = new File("sdcard/Download/test-1.pdf"); // TODO add variable path
+
+        ArrayList<Bitmap> pdfBitmaps = pdfToBitmap(pdfFile);
+
+        for (Bitmap pdfBitmap : pdfBitmaps) // For each bitmap pdf
+        {
+            // Creation of a new imageView
+            imageView = new ImageView(this);
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+
+            imageView.setImageBitmap(pdfBitmap); // Put the bitmap in the imageView
+            linearLayout.addView(imageView); // Put the imageView in the LinearLayout
+        }
+
+        // sView.setOnTouchListener(new ScrollDetect());
+
+        // Animation setup
+        Log.e("GoTo ",""+Math.round(numberPdf * endPage + actionBarHeight - size.y));
+        movePartition = ObjectAnimator.ofInt(sView,"scrollY",0,0);
+        movePartition.addListener(new Animator.AnimatorListener()
+        {
+            public void onAnimationStart(Animator animation) {}
+            public void onAnimationRepeat(Animator animation) {}
+
+            public void onAnimationEnd(Animator animation)
+            {
+                isRunning = false;
+                playPauseButton.setImageResource(R.drawable.ic_play_arrow_white_48dp);
+
+                animatorSet.cancel();
+            }
+            public void onAnimationCancel(Animator animation)
+            {
+                isRunning = false;
+            }
+    });
+        moveRestart = ObjectAnimator.ofInt(sView, "scrollY", Math.round(actionBarHeight), Math.round(actionBarHeight));
+        moveRestart.setDuration(200);
+
+        animatorSet = new AnimatorSet();
 
         textLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
         {
@@ -219,12 +279,12 @@ public class MainActivity extends AppCompatActivity {
                                 else
                                 {
                                 */
-                                    compteurSize++;
-                                    textSize = textSize - 1;
+                                compteurSize++;
+                                textSize = textSize - 1;
 
-                                    // Main Layout margins setup
-                                    artistText.setTextSize(textSize);
-                                    titleText.setTextSize(textSize);
+                                // Main Layout margins setup
+                                artistText.setTextSize(textSize);
+                                titleText.setTextSize(textSize);
                                 // }
 
                                 Log.e("Test oui", "" + endTextLayout + " - " + startHomeLayout);
@@ -243,79 +303,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-        Display display = getWindowManager().getDefaultDisplay();
-        size = new Point();
-        display.getSize(size);
-
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
-        {
-            endScreen = (float) size.x;
-        }
-        else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
-        {
-            endScreen = (float) size.y;
-        }
-
-        linearLayout = (LinearLayout) findViewById(R.id.main_view);
-        sView = (ScrollView) findViewById(R.id.scroll);
-
-        File pdfFile = new File("sdcard/Download/test-1.pdf"); // TODO add variable path
-
-        ArrayList<Bitmap> pdfBitmaps = pdfToBitmap(pdfFile);
-
-        for (Bitmap pdfBitmap : pdfBitmaps) // For each bitmap pdf
-        {
-            // Creation of a new imageView
-            imageView = new ImageView(this);
-            imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-
-            imageView.setImageBitmap(pdfBitmap); // Put the bitmap in the imageView
-            linearLayout.addView(imageView); // Put the imageView in the LinearLayout
-        }
-
-        sView.setOnTouchListener(new ScrollDetect());
-
-        // Animation setup
-        movePartition = ObjectAnimator.ofFloat(linearLayout, "translationY", actionBarHeight, size.y - numberPdf * endPage - actionBarHeight);
-        movePartition.setDuration(3000);
-
-        moveRestart = ObjectAnimator.ofFloat(linearLayout, "translationY", actionBarHeight, endPage - endScreen);
-        moveRestart.setDuration(200);
-
-        movePartition.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                imagePosition = (Float) animation.getAnimatedValue();
-            }
-        });
-
-        animatorSet = new AnimatorSet();
-        animatorSet.play(movePartition);
-        animatorSet.start();
-
-        movePartition.pause();
     }
 
     public void goToHome()
     {
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
-    }
-
-    public void layoutAdaptation()
-    {
-
-    }
-
-    public class ScrollDetect implements View.OnTouchListener
-    {
-        @Override
-        public boolean onTouch(View v, MotionEvent event)
-        {
-            return true;
-        } // Disable scrolling
     }
 
     private ArrayList<Bitmap> pdfToBitmap(File pdfFile) // Convertit un FILE (pdf) en liste de Bitmap
