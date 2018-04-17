2ae64878a62caa5b6520575f60d898d8b65b4213
Basteute <baba.du.22@hotmail.fr>
2018-04-17 14:30:46 +0200

    "parcourir" layout V2 (the speed and music options miss)

--- app/src/main/java/com/autoscroll/fraba/defiloche/ChangePartition.java
+++ app/src/main/java/com/autoscroll/fraba/defiloche/ChangePartition.java
@@ -1,6 +1,7 @@
 package com.autoscroll.fraba.defiloche;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -14,7 +15,9 @@ import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,6 +47,11 @@ public class ChangePartition extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_partition_layout);
 
+        // Layout modification according to orientation of the device
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size); // size.x = device width - size.y = device height
+
         // Toolbar setup
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -51,16 +59,27 @@ public class ChangePartition extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        // Layout modification according to orientation of the device
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size); // size.x = device width - size.y = device height
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) // Portait orientation
+        {
+            size.y = size.y/3;
+        }
+
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) // Landscape orientation
+        {
+            size.x = size.x/2;
+        }
+
+        // Set the RelativeLayout position
+        RelativeLayout relativeLayoutParcourir = (RelativeLayout) findViewById(R.id.relativeLayoutParcourir);
+        relativeLayoutParcourir.setY(convertToFloat(size.y,0.01));
 
         // Set the button at 20% of the screen width
         double buttonWidth = new Double(size.x * 0.2);
         int intButtonWidth = (int) buttonWidth;
         Button ParcourirButton = (Button) findViewById(R.id.ParcourirButton);
-        ParcourirButton.setWidth(intButtonWidth);
+        //ParcourirButton.setWidth(intButtonWidth);
 
         // TextView PDF setup
         TextView partitionNameView = (TextView) findViewById(R.id.partitionNameView);
@@ -68,7 +87,50 @@ public class ChangePartition extends AppCompatActivity {
         if (!pdfChoosen) PDFName = "Partition format PDF";
         partitionNameView.setText(PDFName);
 
+        // Set TextView artiste position
+        TextView artisteTV = (TextView) findViewById(R.id.artisteTV);
+        //artisteTV.setY(convertToFloat(size.y,0.01));
+        //artisteTV.setX(convertToFloat(size.x,0.1));
+
+        /*
+        // Set EditText artiste position
+        EditText artisteED = (EditText) findViewById(R.id.artisteED);
+        artisteED.setY(convertToFloat(size.y,0.4));
+        */
+
+        // Set artisteLayout artiste position
+        LinearLayout artisteLayout = (LinearLayout) findViewById(R.id.artisteLayout);
+        //artisteLayout.setY(convertToFloat(size.y,0.01));
+        //artisteLayout.setX(convertToFloat(size.x,0.02));
+
+
+        /*
+        // Set TextView Titre position
+        TextView titreTV = (TextView) findViewById(R.id.titreTV);
+        titreTV.setY(convertToFloat(size.y,0.5));
+        titreTV.setX(convertToFloat(size.x,0.01));
+
+        // Set titreLayout position
+        LinearLayout titreLayout = (LinearLayout) findViewById(R.id.titreLayout);
+        titreLayout.setY(convertToFloat(size.y,0.5));
+        //titreLayout.setX(convertToFloat(size.x,0.02));
+        */
+
+
+        /*
+        // Set EditText Titre position
+        EditText titreED = (EditText) findViewById(R.id.titreED);
+        titreED.setY(convertToFloat(size.y,0.5));
+        */
+
         //Button validateButton = (Button) findViewById(R.id.validateButton);
+    }
+
+    public float convertToFloat(int nbA, double percentage)
+    {
+        double temp = new Double(nbA * percentage);
+        float result = (float) temp;
+        return result;
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
